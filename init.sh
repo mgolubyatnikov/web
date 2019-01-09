88070ef7d14c06f30102c2d3b5990255fde05d32
Mikhail Golubyatnikov <magolubyatnikov@gmail.com>
2019-01-10 00:24:22 +0300

    django

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
+sudo pip3 uninstall django
+sudo pip3 install django==2.0.0
 sudo unlink /etc/nginx/sites-enabled/default 
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
