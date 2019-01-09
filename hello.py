e2d798ba980d83f4e3fb0191c6c297dc34045833
Mikhail Golubyatnikov <magolubyatnikov@gmail.com>
2019-01-09 16:35:44 +0300

    gunicorn

---
+++ hello.py
@@ -0,0 +1,8 @@
+def application(environ, start_response):
+	query_str = environ['QUERY_STRING']
+	parameters = query_str.split("&")
+	result = "\n".join(parameters)
+    status = '200 OK'
+    response_headers = [('Content-type', 'text/plain')]
+    start_response(status, response_headers)
+    return result
